8de0d0019d27d3868201e39d8f53800fea68f6ee
hindrekhan <hindrek.hannus@tptlive.ee>
2019-03-05 09:19:23 +0200

    Add buttons

--- debt-app/MainActivity.cs
+++ debt-app/MainActivity.cs
@@ -12,6 +12,8 @@ namespace AndroidPager
     public class MainActivity : FragmentActivity
     {
         int count = 1;
+        EditText prices;
+        TextView finalPrice;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -30,6 +32,26 @@ namespace AndroidPager
             adaptor.AddFragmentView((i, v, b) => {
                 var view = i.Inflate(Resource.Layout.person, v, false);
 
+                var item1 = view.FindViewById<ImageView>(Resource.Id.item1);
+                item1.Click += Item1_Click;
+
+                var item2 = view.FindViewById<ImageView>(Resource.Id.item2);
+                item2.Click += Item2_Click;
+
+                var item3 = view.FindViewById<ImageView>(Resource.Id.item3);
+                item3.Click += Item3_Click;
+
+                var item4 = view.FindViewById<ImageView>(Resource.Id.item4);
+                item4.Click += Item4_Click;
+
+                var item5 = view.FindViewById<ImageView>(Resource.Id.item5);
+                item5.Click += Item5_Click;
+
+                var item6 = view.FindViewById<ImageView>(Resource.Id.item6);
+                item6.Click += Item6_Click;
+
+                prices = view.FindViewById<EditText>(Resource.Id.prices);
+
                 //var listView = view.FindViewById<ListView>(Resource.Id.listView);
                 //listView.Adapter = new PeopleAdapter(this, dbService.GetAllPersons());
 
@@ -41,6 +63,8 @@ namespace AndroidPager
 
                 var listView = view.FindViewById<ListView>(Resource.Id.listView);
                 listView.Adapter = new PeopleAdapter(this, dbService.GetAllPersons());
+
+                
 
                 return view;
             });
@@ -58,6 +82,86 @@ namespace AndroidPager
             ActionBar.AddTab(pager.GetViewPageTab(ActionBar, "Person"));
             ActionBar.AddTab(pager.GetViewPageTab(ActionBar, "Asdf"));
             ActionBar.AddTab(pager.GetViewPageTab(ActionBar, "People"));
+        }
+
+        private void Item1_Click(object sender, System.EventArgs e)
+        {
+            prices.Text += "cucumber ";
+            update_text();
+        }
+
+        private void Item2_Click(object sender, System.EventArgs e)
+        {
+            prices.Text += "apple ";
+            update_text();
+        }
+
+        private void Item3_Click(object sender, System.EventArgs e)
+        {
+            prices.Text += "gum ";
+            update_text();
+        }
+
+        private void Item4_Click(object sender, System.EventArgs e)
+        {
+            prices.Text += "beer ";
+            update_text();
+        }
+
+        private void Item5_Click(object sender, System.EventArgs e)
+        {
+            prices.Text += "cigarettes ";
+            update_text();
+        }
+
+        private void Item6_Click(object sender, System.EventArgs e)
+        {
+            prices.Text += "vodka ";
+            update_text();
+        }
+
+        private void update_text()
+        {
+            var items = prices.Text.Split();
+            var finalPrice = 0.0;
+            foreach (var item in items)
+            {
+                if (item == "")
+                    continue;
+
+                switch(item)
+                {
+                    case "cucumber":
+                        finalPrice += 1.0;
+                        break;
+                    case "gum":
+                        finalPrice += 1.0;
+                        break;
+                    case "beer":
+                        finalPrice += 1.0;
+                        break;
+                    case "cigarettes":
+                        finalPrice += 1.0;
+                        break;
+                    case "vodka":
+                        finalPrice += 1.0;
+                        break;
+                    case "apple":
+                        finalPrice += 1.0;
+                        break;
+                    default:
+                        finalPrice = -2.0;
+                        break;
+                }
+
+                if (finalPrice == -2.0)
+                {
+                    prices.Text = "Error";
+                    break;
+                }
+            }
+
+            prices.Text = finalPrice.ToString();
         }
     }
 }
--- debt-app/Resources/Resource.designer.cs
+++ debt-app/Resources/Resource.designer.cs
@@ -2537,8 +2537,8 @@ namespace debt_app
 			// aapt resource value: 0x7f08007e
 			public const int edit_query = 2131230846;
 			
-			// aapt resource value: 0x7f0800ba
-			public const int email = 2131230906;
+			// aapt resource value: 0x7f0800bb
+			public const int email = 2131230907;
 			
 			// aapt resource value: 0x7f080030
 			public const int end = 2131230768;
@@ -2570,6 +2570,9 @@ namespace debt_app
 			// aapt resource value: 0x7f080048
 			public const int fill_vertical = 2131230792;
 			
+			// aapt resource value: 0x7f0800b7
+			public const int finalPrice = 2131230903;
+			
 			// aapt resource value: 0x7f080054
 			public const int @fixed = 2131230804;
 			
@@ -2597,8 +2600,8 @@ namespace debt_app
 			// aapt resource value: 0x7f08005f
 			public const int image = 2131230815;
 			
-			// aapt resource value: 0x7f0800b9
-			public const int imageView1 = 2131230905;
+			// aapt resource value: 0x7f0800ba
+			public const int imageView1 = 2131230906;
 			
 			// aapt resource value: 0x7f08009b
 			public const int img_picture = 2131230875;
@@ -2609,23 +2612,23 @@ namespace debt_app
 			// aapt resource value: 0x7f080059
 			public const int italic = 2131230809;
 			
-			// aapt resource value: 0x7f0800bc
-			public const int item1 = 2131230908;
-			
 			// aapt resource value: 0x7f0800bd
-			public const int item2 = 2131230909;
+			public const int item1 = 2131230909;
 			
 			// aapt resource value: 0x7f0800be
-			public const int item3 = 2131230910;
+			public const int item2 = 2131230910;
 			
-			// aapt resource value: 0x7f0800c0
-			public const int item4 = 2131230912;
+			// aapt resource value: 0x7f0800bf
+			public const int item3 = 2131230911;
 			
 			// aapt resource value: 0x7f0800c1
-			public const int item5 = 2131230913;
+			public const int item4 = 2131230913;
 			
 			// aapt resource value: 0x7f0800c2
-			public const int item6 = 2131230914;
+			public const int item5 = 2131230914;
+			
+			// aapt resource value: 0x7f0800c3
+			public const int item6 = 2131230915;
 			
 			// aapt resource value: 0x7f080000
 			public const int item_touch_helper_previous_elevation = 2131230720;
@@ -2645,14 +2648,14 @@ namespace debt_app
 			// aapt resource value: 0x7f0800b4
 			public const int linearLayout1 = 2131230900;
 			
-			// aapt resource value: 0x7f0800b7
-			public const int linearLayout2 = 2131230903;
+			// aapt resource value: 0x7f0800b8
+			public const int linearLayout2 = 2131230904;
 			
-			// aapt resource value: 0x7f0800bb
-			public const int linearLayout3 = 2131230907;
+			// aapt resource value: 0x7f0800bc
+			public const int linearLayout3 = 2131230908;
 			
-			// aapt resource value: 0x7f0800bf
-			public const int linearLayout4 = 2131230911;
+			// aapt resource value: 0x7f0800c0
+			public const int linearLayout4 = 2131230912;
 			
 			// aapt resource value: 0x7f08001d
 			public const int listMode = 2131230749;
@@ -2663,14 +2666,14 @@ namespace debt_app
 			// aapt resource value: 0x7f080061
 			public const int list_item = 2131230817;
 			
-			// aapt resource value: 0x7f0800c5
-			public const int masked = 2131230917;
+			// aapt resource value: 0x7f0800c6
+			public const int masked = 2131230918;
 			
 			// aapt resource value: 0x7f0800a6
 			public const int media_actions = 2131230886;
 			
-			// aapt resource value: 0x7f0800c3
-			public const int message = 2131230915;
+			// aapt resource value: 0x7f0800c4
+			public const int message = 2131230916;
 			
 			// aapt resource value: 0x7f080031
 			public const int middle = 2131230769;
@@ -2681,8 +2684,8 @@ namespace debt_app
 			// aapt resource value: 0x7f080028
 			public const int multiply = 2131230760;
 			
-			// aapt resource value: 0x7f0800b8
-			public const int name = 2131230904;
+			// aapt resource value: 0x7f0800b9
+			public const int name = 2131230905;
 			
 			// aapt resource value: 0x7f080093
 			public const int navigation_header_container = 2131230867;
@@ -2719,6 +2722,9 @@ namespace debt_app
 			
 			// aapt resource value: 0x7f08004d
 			public const int pin = 2131230797;
+			
+			// aapt resource value: 0x7f0800b3
+			public const int prices = 2131230899;
 			
 			// aapt resource value: 0x7f080006
 			public const int progress_circular = 2131230726;
@@ -2867,9 +2873,6 @@ namespace debt_app
 			// aapt resource value: 0x7f08001b
 			public const int text2 = 2131230747;
 			
-			// aapt resource value: 0x7f0800b3
-			public const int textInputEditText1 = 2131230899;
-			
 			// aapt resource value: 0x7f08006a
 			public const int textSpacerNoButtons = 2131230826;
 			
@@ -2939,8 +2942,8 @@ namespace debt_app
 			// aapt resource value: 0x7f080016
 			public const int view_offset_helper = 2131230742;
 			
-			// aapt resource value: 0x7f0800c4
-			public const int visible = 2131230916;
+			// aapt resource value: 0x7f0800c5
+			public const int visible = 2131230917;
 			
 			// aapt resource value: 0x7f08003c
 			public const int withText = 2131230780;
